01d0c7102cd390f58ac173361d222cf149bcdfcc
kundan kumar <dubeykundan351@gmail.com>
2025-04-02 09:51:40 +0530

    deleted file

--- frontend/New folder/backend/models/cart.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const CartSchema = new mongoose.Schema({
-  userId: String,
-  products: [{ productId: String, quantity: Number }],
-});
-module.exports = mongoose.model("Cart", CartSchema);
